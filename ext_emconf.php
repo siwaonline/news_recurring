f5991e3e1b9147d439e265316530153107f595ad
Tobias Kapplmüller <47564597+siwa-tkapplmueller@users.noreply.github.com>
2022-05-25 13:50:28 +0200

    allow TYPO3 11.5 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.1.99',
+            'typo3' => '10.4.0-11.5.99',
             'news' => '8.4.0-9.99.99',
         ],
         'conflicts' => [],
