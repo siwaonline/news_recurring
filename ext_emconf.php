d2638fcf26d0176706f94ab285b2a1e26704a904
Tobias Kapplmüller <47564597+siwa-tkapplmueller@users.noreply.github.com>
2022-05-25 13:51:25 +0200

    require typo3 11 exclusively

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
-            'news' => '8.4.0-9.99.99',
+            'typo3' => '11.5.0-11.5.99',
+            'news' => '9.0.0-9.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
